1660fa8487f0b1102863951fb7928403de14bd83
nickenilsson <nilsson.sthlm@gmail.com>
2016-01-31 14:41:48 +0100

    Added url validation and error handling

--- handlers/user_lists.py
+++ handlers/user_lists.py
@@ -1,10 +1,18 @@
 from tornado import web
 from pymongo import MongoClient
+import time
+import validators
 
 class UserListHandler(web.RequestHandler):
 	def __init__(self, *args, **kwargs):
 		super(UserListHandler, self).__init__(*args, **kwargs)
 		self.mongo_client = MongoClient()
+
+
+	def write_error(self, status_code, **kwargs):
+		self.set_status(**kwargs)
+		self.finish()
+
 
 	def get(self):
 		user_id = str(self.get_argument('userID'))
@@ -24,9 +32,18 @@ class UserListHandler(web.RequestHandler):
 
 	def post(self):
 
+		image_url = self.get_argument('imageUrl', None)
+		if image_url:
+			try:
+				validators.url(image_url)
+			except validators.ValidationFailure, e:
+				self.write_error({'status_code': 400, 'message': 'Invalid url'})
+
 		wishlist_item = {
 			'userID': self.get_argument('userID'),
 			'title': self.get_argument('title'),
-			'imageUrl': self.get_argument('imageUrl')
+			'imageUrl': self.get_argument('imageUrl'),
+			'createdAt': time.time()
 		}
 		self.mongo_client.wishlist.user.insert(wishlist_item)
+
