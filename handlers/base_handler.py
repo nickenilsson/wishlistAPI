7644cf2e8133b57ebba2fa0f73b3f74aba70ecb5
nickenilsson <nilsson.sthlm@gmail.com>
2016-02-14 11:54:17 +0100

    Changed mongo host to localhost instead of local ip

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -6,7 +6,7 @@ class BaseHandler(web.RequestHandler):
 
 	def __init__(self, *args, **kwargs):
 		super(BaseHandler, self).__init__(*args, **kwargs)
-		self.list_helper = mongodb_helper.ListHelper('54.84.3.211')
+		self.list_helper = mongodb_helper.ListHelper('localhost')
 
 	def display_error(self, status_code, message):
 		self.set_status(status_code)
