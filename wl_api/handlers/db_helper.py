142ca8745b499afc7cf1bcce03e5a025eae4fdd6
nickenilsson <nilsson.sthlm@gmail.com>
2016-05-22 12:02:54 +0200

    added async deletion for wishlists. Also added some error handling

--- wl_api/handlers/db_helper.py
+++ wl_api/handlers/db_helper.py
@@ -98,14 +98,19 @@ class DBHelper(object):
         article['_id'] = ObjectId()
 
         query = {'_id': wish_list_id, '_author_id':user_id}
-        self.mongo_client.wishlists.update_one(query, {'$push': {'articles': article}})
+        result = self.mongo_client.wishlists.update_one(query, {'$push': {'articles': article}})
+        if result.matched_count is not 1:
+            raise tornado.web.HTTPError(400, "No matching documents found")
         return str(article['_id'])
 
     #TODO: Make sure that only the author can edit articles
     def update_article(self, article):
         article = self._fix_doc_before_insert(article.store)
-        return self.mongo_client.wishlists.update_one( {'articles._id':article['_id']},
+        result = self.mongo_client.wishlists.update_one( {'articles._id':article['_id']},
                                                 {'$set':{'articles.$.{0}'.format(k):v for k, v in article.items()}})
+        if result.matched_count != 1:
+            raise tornado.web.HTTPError(400, "No matching documents found")
+        return result
 
     def update_wishlist(self, wishlist_id, wishlist):
         wishlist_id = ObjectId(wishlist_id) if not isinstance(wishlist_id, ObjectId) else wishlist_id
@@ -115,20 +120,27 @@ class DBHelper(object):
 
     def delete_article(self, article_id):
         article_id = ObjectId(article_id) if not isinstance(article_id, ObjectId) else article_id
-        return self.mongo_client.wishlists.update_one({'articles._id': article_id}, {'$pull': {'articles':{'_id': article_id}}})
+        result = self.mongo_client.wishlists.update_one({'articles._id': article_id}, {'$pull': {'articles':{'_id': article_id}}})
+        if result.matched_count is not 1:
+            raise tornado.web.HTTPError(400, "No matching documents found")
+        return result
 
 
     def delete_wishlist(self, wish_list_id, user_id):
         wish_list_id = ObjectId(wish_list_id) if not isinstance(wish_list_id, ObjectId) else wish_list_id
         user_id = ObjectId(user_id) if not isinstance(user_id, ObjectId) else user_id
-        self.mongo_client.wishlists.remove({'_id': wish_list_id, '_author_id': user_id})
+        del_result = self.mongo_client.wishlists.delete_one({'_id': wish_list_id, '_author_id': user_id})
+        print del_result
+        if del_result.deleted_count != 1:
+            raise tornado.web.HTTPError(400, "No documents found to delete")
         self.mongo_client.users.update({'_id': user_id},{'$pull':{'wishlists':wish_list_id}})
-        delete_wishlist_from_all_users(self.mongo_client, wish_list_id)
+        q = Queue(connection=Redis())
+        q.enqueue(delete_wishlist_from_all_users, wish_list_id)
 
 
 def delete_wishlist_from_all_users(mongo_client, wishlist_id):
     wishlist_id = ObjectId(wishlist_id) if not isinstance(wishlist_id, ObjectId) else wishlist_id
-    mongo_client.users.update({'wishlists': wishlist_id}, {'$pull': {'wishilsts': wishlist_id}}, multi=True)
+    mongo_client.users.update({'wishlists': wishlist_id}, {'$pull': {'wishlists': wishlist_id}}, multi=True)
 
 
 
