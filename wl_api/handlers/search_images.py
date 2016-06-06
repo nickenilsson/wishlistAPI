cd4c17a40780b5ec86215a9bcb161095eccbe8d1
nickenilsson <nilsson.sthlm@gmail.com>
2016-06-06 15:51:56 +0200

    jerp

--- wl_api/handlers/search_images.py
+++ wl_api/handlers/search_images.py
@@ -11,7 +11,7 @@ class SearchImagesHandler(BaseHandler):
     GOOGLE_API_KEY = 'AIzaSyBdV1Emfi7Zd-T8_PQ1tr_Av7G4WuGpHOo'
     API_ID = '012147580588716782987:8xdixcfdasg'
     FIELDS = 'items(pagemap/cse_image)'
-    GOOGLE_SEARCH_URL_TEMPLATE = 'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={api_id}&q={query}&fields={fields}&searchType=image'
+    GOOGLE_SEARCH_URL_TEMPLATE = 'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={api_id}&q={query}&fields={fields}&searchType="image"'
 
     @tornado.web.authenticated
     def get(self):
@@ -29,9 +29,11 @@ class SearchImagesHandler(BaseHandler):
         )
 
         response = requests.get(search_url)
+
         response.raise_for_status()
         response_dict = json.loads(response.text)
-
+        import json
+        print json.dumps(response_dict)
         images = [Image(url=r['pagemap']['cse_image'][0]['src']).store for r in response_dict['items']]
         self.write({'response': {'images':images}})
         self.finish()
